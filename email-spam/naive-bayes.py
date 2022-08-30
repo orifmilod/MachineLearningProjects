8b97772639d6bd0e7d2012b15d48661dbb6d77aa
orifmilod <orifmilod@gmail.com>
2022-08-30 16:46:32 +0200

    Implement NaiveBayes::predict function

--- email-spam/naive-bayes.py
+++ email-spam/naive-bayes.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 import string
 import nltk
-
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
-def remove_punctuation(text):
+def remove_punctuation_and_lower(text):
     return "".join([char for char in text if char not in string.punctuation]).lower()
 
 def remove_stopwords(word_vector):
@@ -18,12 +16,61 @@ def tokenize(text):
 
 
 class NaiveBayes:
-    def predict():
-        pass
+    # Returns the probability of the words given that it's spam or not spam
+    def __init__(self, min_word_occurance):
+      self.MIN_WORD_OCCURANCE = min_word_occurance
 
-    def fit():
-        pass
+    def P_words(self, text_list, labels):
+      if(len(text_list) != len(labels)):
+        raise Exception("Inputs not same length")
 
+      dict_prob = {} # For each word in vocabulary consist P(word|!spam) and P(word|spam)
+
+      # Count number of words for both spam and not spam
+      for i in range(len(text_list)):
+        text = text_list[i][0] # wtf is this, TODO: fix it
+        label = labels[i]
+        is_spam = True if label == 1 else False
+
+        words = text.split()
+        for word in words:
+          # First time seeing this word
+          if word not in dict_prob:
+            dict_prob[word] = {"spam": 0, "not_spam": 0}
+
+          dict_prob[word]['spam' if is_spam else 'not_spam'] += 1
+
+      # Calculating probability
+      # print(dict_prob)
+
+      for key in dict_prob:
+        total = dict_prob[key]['spam'] + dict_prob[key]['not_spam']
+        # TODO: Check diff values
+        # if(total < self.MIN_WORD_OCCURANCE): # Ignore words which have little amount of occurance
+          # del dict_prob[key]
+        # else:
+        dict_prob[key]['spam_prob'] = dict_prob[key]['spam'] / total
+        dict_prob[key]['not_spam_prob'] = dict_prob[key]['not_spam'] / total
+        del dict_prob[key]['spam']
+        del dict_prob[key]['not_spam']
+
+      print(dict_prob)
+      return dict_prob
+
+    def predict(self, X_test, y_test):
+      pass
+
+    def fit(self, X_train, y_train):
+      self.X_train = X_train
+      self.y_train = y_train
+
+      # Building a prio
+      _, counter = np.unique(y_train, return_counts=True)
+      self.non_spam_prob = counter[1] / len(y_train)
+      self.spam_prob = counter[0] / len(y_train)
+
+      # Creating the Bag of Words model
+      self.dict_prob = self.P_words(X_train, y_train)
 
 
 def main():
@@ -33,29 +80,19 @@ def main():
     # Data cleaning
     data = data.drop_duplicates(keep = 'last')
 
-    data['text'] = data['text'].apply(lambda text:remove_punctuation(text))
+    data['text'] = data['text'].apply(lambda text:remove_punctuation_and_lower(text))
     data['text'] = data['text'].apply(lambda text:tokenize(text))
     data['text'] = data['text'].apply(lambda text:remove_stopwords(text))
     data['text'] = data['text'].apply(lambda text: " ".join(text))
 
-    # print(data.head())
-
     X = data.drop(columns=['spam'])
     y = data.spam
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-    corpus = X_train['text'].to_numpy()
+    nb = NaiveBayes(3)
+    nb.fit(X_train.to_numpy(), y_train.to_numpy())
+    nb.predict(X_test, y_test)
 
-    # Uni-gram
-    unigram_vectorizer = CountVectorizer(min_df = 50) # Ignoring words occuring less than 50 times 
-    C = unigram_vectorizer.fit_transform(corpus)
-    print(len(unigram_vectorizer.get_feature_names()))
-
-    #Bi-gram
-    bigram_vectorizer = CountVectorizer(analyzer='word', ngram_range=(2, 2), min_df = 50)
-    C2 = bigram_vectorizer.fit_transform(corpus)
-    print(len(bigram_vectorizer.get_feature_names()))
 
 if __name__ == "__main__":
     main()
